298f4348444d16e8e212fedf66871f494019aee6
Adriano <adriano.garcia-giraldamilena@unisalento.studenti.it>
2022-06-23 10:30:28 +0200

    Modifiying the server file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const sensorLib = require('node-dht-sensor'); // include existing module called 'node-dht-sensor'
-const http = require('http')
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+app.post("/temperature", (req, res, next) => {
+    console.log("receivedData");
+    res.sendStatus(200);
+});
