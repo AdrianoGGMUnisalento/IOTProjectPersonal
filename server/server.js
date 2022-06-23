8c6d29f3b5c81aa8d6913260f90d640c0b3d84be
Adriano <adriano.garcia-giraldamilena@unisalento.studenti.it>
2022-06-23 10:40:33 +0200

    Modifiying the server file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,48 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb://localhost/TemperatureDB';
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json());
 app.post("/temperature", (req, res, next) => {
-    console.log("receivedData");
-    res.sendStatus(200);
+    console.log(req.body.temperature);
+    var temperature = req.body.temperature;
+    var timestamp = req.body.timestamp;
+    var sensor = req.body.sensor;
+
+    const client = new MongoClient(uri, {useUnifiedTopology: true});
+    async function run() {
+
+        try {
+
+            await client.connect();
+
+            const database = client.db("TemperatureDB");
+            const temperatureColl = database.collection("temperature");
+            // create a document to be inserted
+            const doc = {
+                value: temperature,
+                timestamp: timestamp,
+                sensorId: sensor,
+                roomId: 'room1'
+            };
+
+            const result = await temperatureColl.insertOne(doc);
+            console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,);
+        } finally {
+            await client.close();
+        }
+    }
+
+    run().catch(console.dir);
+    res.sendStatus(200)
 });
